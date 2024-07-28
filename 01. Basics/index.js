94e49623a01cd0a8e831378a8ae870a5fa692134
amolpalwe2002 <amolpalwe2002@gmail.com>
2024-07-28 15:41:58 +0530

    Day01 JS Basics

---
+++ 01. Basics/index.js
@@ -0,0 +1,150 @@
+// // print in command line
+// console.log("Hello World");
+
+// // alert in browser
+// alert("Hello World");
+
+// // print in browser
+// document.write("Hello World");
+
+// // types of console log
+// console.log("Hello World");
+// console.warn("Hello World");
+// console.error("Hello World");
+// console.info("Hello World");
+// console.assert("Hello World");
+// console.table({name: "John", age: 30});
+
+// // clg -> console.log
+// // cwa -> console.warn
+// // cwe -> console.error
+// // cii -> console.info
+// // caa -> console.assert
+// // cta -> console.table
+
+// // variables
+// // rules for naming variables
+// // 1. variable name should not start with number
+// // 2. variable name should not contain special characters except '$' and '_'
+// // 3. variable name should not be same as keywords
+
+// // types of variables
+// let name = "John";
+// let age = 30;
+// let isMarried = false;
+// let languages = ["English", "French", "Spanish"];
+// let person = {  name: "John", age: 30, isMarried: false };
+// let address = null;
+// let nothing = undefined;    
+
+// // console.log  
+// console.log(name, age, isMarried, languages, person, address, nothing);
+
+// // types of declaring a variable
+// // snake case
+// let first_name = "John";
+
+// // camel case
+// let firstName = "John";
+
+// // pascal case
+// let FirstName = "John";
+
+// // constants
+// const name = "John";
+
+// // data types
+// let name = "John";
+// let age = 30;
+// let isMarried = false;
+// let languages = ["English", "French", "Spanish"];
+// let person = {  name: "John", age: 30, isMarried: false };
+// let address = null;
+// let nothing = undefined;
+
+// // typesOf data types
+// console.log(typeof name, typeof age, typeof isMarried, typeof languages, typeof person, typeof address, typeof nothing);
+
+// // boolean
+// let isMarried = true;
+// let isSingle = false;
+
+// console.log(isMarried, isSingle);
+
+// // comparison operators
+// let num1 = 10;
+// let num2 = 20;
+
+// console.log(num1 > num2);
+// console.log(num1 < num2);
+// console.log(num1 >= num2);
+// console.log(num1 <= num2);
+// console.log(num1 == num2);
+// console.log(num1 === num2);
+// console.log(num1 != num2);
+// console.log(num1 !== num2);
+
+// // Strings 
+// let name = "John";
+// let name = 'John';
+// let age = 30;
+// let name = `John- ${age}`;
+
+// console.log(name);
+
+// string operations
+// let name = "John";
+// let age = 30;    
+
+// console.log(name + " " + age);
+
+// string methods
+// let name = "John";
+
+// console.log(name.length);
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+// console.log(name.trim());
+// console.log(name.split(" "));
+
+// #################################################################
+// // Challege 1
+// let name = "John";
+// let whatDoYouWannaBecome = "Programmer";
+// let gender = "Male";
+// let githubUsername = "john-doe";
+
+// console.log(name, whatDoYouWannaBecome, gender, githubUsername);
+
+// // // Challege 2
+// let firstFavNum = 5;
+// let secondFavNum = 10;
+// let addRes = firstFavNum + secondFavNum;
+// let subRes = firstFavNum - secondFavNum;
+// let mulRes = firstFavNum * secondFavNum;
+// let divRes = firstFavNum / secondFavNum;
+// let modRes = firstFavNum % secondFavNum;
+// let expRes = firstFavNum ** secondFavNum;
+
+// console.log(addRes, subRes, mulRes, divRes, modRes, expRes);
+
+// // Challege 3
+// let isJsProgrammingLanguage = true;
+// let isJsHard = false;
+// let favNum = 7;
+// let res = favNum + undefined;
+
+// console.log(isJsProgrammingLanguage, isJsHard, favNum, res);
+
+// // Challenge 4
+// let firstFavNum = 5;
+// let secondFavNum = 10;
+// console.log(firstFavNum > secondFavNum);
+// console.log(firstFavNum < secondFavNum);
+// console.log(firstFavNum >= secondFavNum);
+// console.log(firstFavNum <= secondFavNum);
+// console.log(firstFavNum == secondFavNum);
+// console.log(firstFavNum === secondFavNum);
+// console.log(firstFavNum != secondFavNum);
+// console.log(firstFavNum !== secondFavNum);
+
